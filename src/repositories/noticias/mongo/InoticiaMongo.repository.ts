cf29ac0f179e7d686e5efc720f64c5616c1581b0
ASuelvesE <210059@fppiramide.com>
2022-11-24 11:59:15 +0100

    falta swagger

--- src/repositories/noticias/mongo/InoticiaMongo.repository.ts
+++
@@ -1,6 +0,0 @@
-import Noticia,{INoticia} from "../../../models/Noticia"
-
-export default interface INoticiaMongoRepository{
-    findAll() : Promise<INoticia[]>;
-    findByAutor(autor: string) : Promise<INoticia[]>;
-}
--- src/routes/noticias.router.ts
+++ src/routes/noticias.router.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express'
-import NoticiaMongoRepository from '../repositories/noticias/mongo/noticiaMongoRepository'
+import ApiNoticiaMongoRepository from '../repositories/api/noticias/api.noticia.repository.mongo'
 import Noticia, { INoticia } from '../models/Noticia'
 import ApiPeriodistasRepositoryMySQL from '../repositories/api/periodistas/api.periodistas.repositorymysql'
 import ApiRecursosRepositoryMySQL from '../repositories/api/recursos/api.recursos.repositorymysql'
@@ -9,7 +9,7 @@ import Periodista from '../models/Periodista'
 const router = express.Router()
 
 
-const noticiasRepository = new NoticiaMongoRepository()
+const noticiasRepository = new ApiNoticiaMongoRepository()
 const IApiPeriodistasRepository = new ApiPeriodistasRepositoryMySQL()
 const ApiRecursosRepository = new ApiRecursosRepositoryMySQL()
 
@@ -44,8 +44,6 @@ router.get('/', async (req: Request, res: Response) => {
         res.render('pages/noticias', {
             noticias
         });
-
-
     }
     catch (error) {
         res.send(error)
@@ -53,10 +51,35 @@ router.get('/', async (req: Request, res: Response) => {
 })
 router.get('/:periodista', async (req: Request, res: Response) => {
     try {
-        const noticias: INoticia[] = await noticiasRepository.findByAutor(req.params.periodista)
-        res.render('pages/noticias', {
-            noticias: noticias
-        });
+        const noticiasDB: any[] = await noticiasRepository.findByIdPeriodista(Number(req.params.periodista))
+        const noticias: INoticia[] = [];
+        for (let noticiaDB of noticiasDB) {
+            const noticia: INoticia = {
+                id: noticiaDB._id,
+                titulo: noticiaDB.titulo,
+                texto: noticiaDB.texto,
+                periodistas: [],
+                recursos: []
+            }
+            for (let recurso of noticiaDB.recursos) {
+                const recursos: Recurso[] = await ApiRecursosRepository.findById(Number(recurso))
+                for (let recurso of recursos) {
+
+                    noticia.recursos.push(new Recurso(recurso.id, recurso.url))
+                }
+            }
+            for (let periodista of noticiaDB.periodistas) {
+                const periodistas: Periodista[] = await IApiPeriodistasRepository.findById(Number(periodista))
+                for (let periodista of periodistas) {
+                    const nuevo: Periodista = new Periodista(0,periodista.nombre,periodista.fechaNacimiento,undefined)
+                   noticia.periodistas.push(nuevo)
+                }
+            }
+            noticias.push(noticia)
+        }
+        res.render('pages/noticiasPeriodista', {
+            noticias
+        })
     }
     catch (error) {
         res.send(error)
